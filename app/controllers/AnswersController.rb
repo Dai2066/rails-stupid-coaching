ada75e299ad342eeaa8377f171399558da675642
Daiana Cergneux <cergneux.daiana@gmail.com>
2021-06-21 13:14:15 -0300

    backend coach app

---
+++ app/controllers/AnswersController.rb
@@ -0,0 +1,14 @@
+class AnswersController < ApplicationController
+  def answer 
+    if params[:question].present?
+      @answer = ""
+      if params[:question] == 'I am going to work'
+        @answer = "Great!"        
+      elsif params[:question].include? "?"
+        @answer = "Silly question, get dressed and go to work!"
+      else 
+        @answer = "I don't care, get dressed and go to work!"  
+      end
+    end
+  end    
+end
